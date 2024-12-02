ee2e081b96766785aff6298ef0390a7f1b0c453f
Andrey <aprishlov@gmail.com>
2024-12-02 23:41:38 +0300

    feat: added PropertiesUtils

---
+++ src/main/java/prog/rohan/currency_conversion/utils/PropertiesUtil.java
@@ -0,0 +1,27 @@
+package prog.rohan.currency_conversion.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class PropertiesUtil {
+    private static final Properties PROPERTIES = new Properties();
+
+    static{
+        loadProperties();
+    }
+
+    private PropertiesUtil(){}
+
+    public static String get(String key){
+        return PROPERTIES.getProperty(key);
+    }
+
+    private static void loadProperties() {
+        try(InputStream inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream("aplication.properties")){
+            PROPERTIES.load(inputStream);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
